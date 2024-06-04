e32dc65acc713a3267ac03a080a8fb80da9ef7dc
Sulthonhanan <hananakhnaf31@gmail.com>
2024-06-04 15:18:54 +0700

    Menambahkan Fitur Pengecualian untuk Integer

---
+++ ManualException/ManualException.cpp
@@ -0,0 +1,21 @@
+// ManualException.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+   try
+   {
+	   cout << "Selamat Belajar di Prodi TI UMY" << endl;
+	   // throw 0.5; // melemparkan sebuah integer maka
+	   cout << "Pernyataan tidak akan dieksekusi" << endl;
+   }
+   catch (int a)
+   {
+	   // blok ini akan dieksekusi
+	   cout << "Pengecualian akan dieksekusi" << endl;
+   }
+}
+
