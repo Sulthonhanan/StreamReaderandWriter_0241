40452c1e285f4e45f1a9e92f698276ace5dd25f5
Sulthonhanan <hananakhnaf31@gmail.com>
2024-06-04 15:07:04 +0700

    Menambahkan Fitur Membaca File yang Telah Dibuat dengan Penanganan Error

--- ExceptionStandard/ExceptionStandard.cpp
+++ ExceptionStandard/ExceptionStandard.cpp
@@ -18,3 +18,14 @@ int main()
 		// cout << data.at(5) << endl;
 		// memanggil array elemen ke 5
 	}
+	catch (exception& e)
+	{
+		// penangkap menggynakan obyej exception
+		cout << e.what() << endl;
+		// akan dieksekusi karena array data hanya memiliki 3 elemen
+	}
+	cout << "Baris Program Yang terakhir" << endl;
+	// penanda 2: bahwa program berjalan tanpa berhenti meskipun terjadi kesalahan
+	return 0;
+}
+
